1e046107329cdf431e881aa5bed4ce32514d8e11
CyberMage7 <technician9012@gmail.com>
2024-12-23 14:05:56 +0530

    Added Header section

--- Xora/src/App.jsx
+++ Xora/src/App.jsx
@@ -1,11 +1,9 @@
-import React from 'react'
-
+import React from "react";
+import Header from "./sections/Header";
 function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+  return <main className="overflow-hidden">
+    <Header />
+  </main>;
 }
 
-export default App
+export default App;

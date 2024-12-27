7f5311052dec02fd685efd2e4814267c3653b644
CyberMage7 <technician9012@gmail.com>
2024-12-27 17:55:06 +0530

    Added the Hero section

--- Xora/src/App.jsx
+++ Xora/src/App.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import Header from "./sections/Header";
+import Hero from "./sections/Hero";
 function App() {
   return <main className="overflow-hidden">
     <Header />
+    <Hero />
   </main>;
 }
 

2cc32eb3cf2e506beabfc0b6f86b855aadba01f7
CyberMage7 <technician9012@gmail.com>
2024-12-27 18:40:24 +0530

    Added features section

--- Xora/src/App.jsx
+++ Xora/src/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Header from "./sections/Header";
 import Hero from "./sections/Hero";
+import Features from "./sections/Features";
 function App() {
-  return <main className="overflow-hidden">
-    <Header />
-    <Hero />
-  </main>;
+  return (
+    <main className="overflow-hidden">
+      <Header />
+      <Hero />
+      <Features />
+    </main>
+  );
 }
 
 export default App;
